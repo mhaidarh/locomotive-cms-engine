7bda6b8e4b2b8cda63a7f9a251075ed4ac682273
M Haidar Hanif <mhaidarhanif@gmail.com>
2015-05-15 21:36:12 +0700

    Enable more file formats and /admin URL

--- config/initializers/engine_overrides.rb
+++ config/initializers/engine_overrides.rb
@@ -1,6 +1,6 @@
 Locomotive::ThemeAssetUploader.class_eval do
   def extension_white_list
-    %w(jpg jpeg gif png css js swf flv eot svg ttf woff woff2 otf ico htc pdf xml json)
+    %w(jpg jpeg gif png css js swf flv eot svg ttf woff woff2 otf ico htc pdf xml json md html txt)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 HaidokterEngineRails3::Application.routes.draw do
   
-  mount Locomotive::Engine => '/locomotive', as: 'locomotive' # you can change the value of the path, by default set to "/locomotive"
-      
+	# you can change the value of the path, by default set to "/locomotive"
+  mount Locomotive::Engine => '/admin', as: 'locomotive'    
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
